fc9a24c5ccf2f9782f0e1fa5e4db333b686df89c
Max Wood <45072525+bridges-wood@users.noreply.github.com>
2021-03-05 17:20:11 +0000

    Initial commit.

---
+++ src/main/java/tempest/interfaces/CSVInterface.java
@@ -0,0 +1,5 @@
+package tempest.interfaces;
+
+public class CSVInterface {
+
+}
---
+++ src/test/java/tempest/interfaces/CSVInterfaceTest.java
@@ -0,0 +1,18 @@
+package tempest.interfaces;
+
+import org.junit.Test;
+
+public class CSVInterfaceTest {
+  private CSVInterface i = new CSVInterface();
+
+  @Test
+  public void findsFile() {
+
+  }
+
+  @Test
+  public void readsState() {
+
+  }
+
+}
