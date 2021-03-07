0937d836a398c7a46f16f76d325e09970bbcbf0e
JoshRenton <78546327+JoshRenton@users.noreply.github.com>
2021-03-07 23:10:01 +0000

    Made the DataEntry class.

--- src/test/java/DataEntryGUI/DataEntryGUITest.java
+++ src/test/java/DataEntryGUI/DataEntryGUITest.java
@@ -1,4 +1,15 @@
 package DataEntryGUI;
 
+import org.junit.Test;
+import tempest.DataEntry;
+
+import static org.junit.Assert.*;
+
 public class DataEntryGUITest{
+    
+    //Test that the data input by the user is output by the class
+    @Test
+    public void testDataOutput(){
+
+    }
 }
