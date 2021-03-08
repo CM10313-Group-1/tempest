a1486f9ddb0036735195ee22f9f702f4d76848a0
JoelyMo101 <6833565+JoelyMo101@users.noreply.github.com>
2021-03-08 17:35:31 +0000

    Add initial Module Model and tests

---
+++ src/main/java/tempest/Module.java
@@ -0,0 +1,36 @@
+package tempest;
+
+import java.time.Duration;
+import java.util.Date;
+import java.util.LinkedList;
+
+/**
+ * Hello world!
+ *
+ */
+public class Module {
+    public String id;
+    public String moduleName;
+    public LinkedList<StudySession> studySessionsList;
+
+    public Module(){}
+
+    public Module(String id, String moduleName){
+        this.id = id;
+        this.moduleName = moduleName;
+    }
+
+    public Module(String id, String moduleName, LinkedList<StudySession> studySessionsList){
+        this.id = id;
+        this.moduleName = moduleName;
+        this.studySessionsList = studySessionsList;
+    }
+
+    public void addSession(Date date, Duration duration){
+        studySessionsList.add(new StudySession(date, duration));
+    }
+
+    public void addSession(StudySession session){
+        studySessionsList.add(session);
+    }
+}
--- src/test/java/tempest/ModuleModelTest.java
+++ src/test/java/tempest/ModuleModelTest.java
@@ -4,16 +4,29 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.time.Duration;
+import java.util.Date;
+
 /**
  * Unit test for simple App.
  */
-public class ModuleModelTest 
-{
+public class ModuleModelTest {
     private ModuleModelTest i = new ModuleModelTest();
 
     @Test
-    public void addSession()
-    {
-        
+    public void addSessionByCreatingSession() {
+        Module module = new Module();
+        StudySession today = new StudySession(new Date(), Duration.ofHours(1));
+        module.addSession(today);
+        assertEquals(today, module.studySessionsList.get(-1));
+    }
+
+    @Test
+    public void addSessionWithDateDuration() {
+        Module module = new Module();
+        Date date = new Date();
+        module.addSession(date, Duration.ofHours(1));
+        assertEquals(date, module.studySessionsList.get(-1).date);
+        assertEquals(Duration.ofHours(1), module.studySessionsList.get(-1).duration);
     }
 }
