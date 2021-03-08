142b9909cd53869fd91dd22e90de8a501d77c6a4
Max Wood <45072525+bridges-wood@users.noreply.github.com>
2021-03-08 17:11:02 +0000

    Added javadoc.

--- src/main/java/tempest/interfaces/CSVInterface.java
+++ src/main/java/tempest/interfaces/CSVInterface.java
@@ -6,6 +6,15 @@ import java.net.URISyntaxException;
 import java.net.URL;
 
 public class CSVInterface {
+
+  /**
+   * Acquires a given file in the <code>resources</code> directory.
+   * 
+   * @param filename The filename to be acquired.
+   * @return A file object representing the acquired file.
+   * @throws FileNotFoundException
+   * @throws URISyntaxException
+   */
   protected File getFile(String filename) throws FileNotFoundException, URISyntaxException {
     URL resource = getClass().getClassLoader().getResource(filename);
     if (resource == null) {
