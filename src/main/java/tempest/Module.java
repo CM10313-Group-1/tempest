11d11c8a2f997094b535130f722ead4e95f6234d
JoelyMo101 <6833565+JoelyMo101@users.noreply.github.com>
2021-03-08 17:53:28 +0000

    Add javadoc to Module class

--- src/main/java/tempest/Module.java
+++ src/main/java/tempest/Module.java
@@ -25,11 +25,21 @@ public class Module {
         this.moduleName = moduleName;
         this.studySessionsList = studySessionsList;
     }
-
+    
+    /**
+     * This method adds a study session to the sessions list of the module.
+     * It creates a StudySession using the date and duration arguments.
+     * @param date : Date of session
+     * @param duration : Duration of session
+     */
     public void addSession(Date date, Duration duration){
         studySessionsList.add(new StudySession(date, duration));
     }
 
+    /**
+     * This method adds a study session to the sessions list of the module.
+     * @param session : StudySession created before calling this method
+     */
     public void addSession(StudySession session){
         studySessionsList.add(session);
     }
