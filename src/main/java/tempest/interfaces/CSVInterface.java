daddcc4133946a26aaafa62f8b3dfb425fa37b54
Max Wood <45072525+bridges-wood@users.noreply.github.com>
2021-03-08 17:07:29 +0000

    Added getFile.

--- src/main/java/tempest/interfaces/CSVInterface.java
+++ src/main/java/tempest/interfaces/CSVInterface.java
@@ -1,5 +1,17 @@
 package tempest.interfaces;
 
-public class CSVInterface {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
+public class CSVInterface {
+  protected File getFile(String filename) throws FileNotFoundException, URISyntaxException {
+    URL resource = getClass().getClassLoader().getResource(filename);
+    if (resource == null) {
+      throw new FileNotFoundException(filename + " does not exist in resources directory.");
+    } else {
+      return new File(resource.toURI());
+    }
+  }
 }
--- src/test/java/tempest/interfaces/CSVInterfaceTest.java
+++ src/test/java/tempest/interfaces/CSVInterfaceTest.java
@@ -1,14 +1,28 @@
 package tempest.interfaces;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.junit.Assume.assumeTrue;
+
+import java.io.File;
+
 import org.junit.Test;
 
 public class CSVInterfaceTest {
   private CSVInterface i = new CSVInterface();
-  String fixtureLocation = "";
+  String fixtureLocation = "test.csv";
 
   @Test
   public void findsExistingFile() {
-
+    try {
+      File found = i.getFile(fixtureLocation);
+      assumeTrue("File does not exist", found.exists());
+      assertEquals("Names should match", found.getName(), fixtureLocation);
+      assumeTrue("Cannot read to file", found.canRead());
+      assumeTrue("Cannot write to file", found.canWrite());
+    } catch (Exception e) {
+      fail("File should exist");
+    }
   }
 
   @Test
