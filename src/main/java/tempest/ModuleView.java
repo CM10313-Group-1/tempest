9913e4a890923e5115be1beed79d8f9384517ab6
Drayz64 <dwtr20@bath.ac.uk>
2021-03-10 17:00:39 +0000

    Renamed Test class to Module View

--- src/main/java/tempest/ModuleView.java
+++ src/main/java/tempest/ModuleView.java
@@ -1,75 +1,199 @@
 package tempest;
 
-import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import javax.swing.*;
 
-public class ModuleView extends JFrame{
+public class ModuleView extends JFrame implements ActionListener {
 
-    //could create the 'home' menu as soon as the class constructor is called
+    private int currentCard = 1;
 
-    public static void main(String[] args){
+    private JPanel cardPanel;
+    private JPanel buttonPanel = new JPanel();
+
+    private JTextField createModuleInput;
+
+    private JTextField hoursInput;
+    private JTextField minutesInput;
+    private JComboBox<Object> moduleDropDown;
+
+    private JButton cancelButton;
+    private JButton enterButton;
+
+    private CardLayout cl;
+
+    public ModuleView()
+    {
+        setTitle("Tempest");
+
+        setSize(500, 150);
+
+        cardPanel = new JPanel();
+
+        cl = new CardLayout();
+
+        // set the layout
+        cardPanel.setLayout(cl);
+
+        // **** Home Panel ****
+        JPanel home = new JPanel();
+
+        home.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JButton addModuleButton  = new JButton("Add a new module");
+        JButton addSessionButton = new JButton("Add a new session");
+
+        addModuleButton.addActionListener(this);
+        addSessionButton.addActionListener(this);
+
+        home.add(addModuleButton);
+        home.add(addSessionButton);
+
+        addModuleButton.addActionListener(arg0 -> {
+            currentCard = 2;
+
+            cl.show(cardPanel, Integer.toString(currentCard));
+
+            cancelPanelVisible(currentCard);
+        });
+
+        addSessionButton.addActionListener(arg0 -> {
+            currentCard = 3;
+
+            cl.show(cardPanel, Integer.toString(currentCard));
+
+            cancelPanelVisible(currentCard);
+        });
+
+
+        // **** Add Module Panel ****
+        JPanel addModule = new JPanel();
+
+        JPanel createModulePanel = new JPanel();
+
+        JLabel createModuleLabel = new JLabel("Enter module name:");
+        createModuleInput = new JTextField(20);
+
+        createModulePanel.add(createModuleLabel);
+        createModulePanel.add(createModuleInput);
+        createModulePanel.setLayout(new FlowLayout());
+
+        addModule.add(createModulePanel);
+
+        // **** Add Session Panel
+        JPanel addSession = new JPanel();
+
         ArrayList<String> modules = new ArrayList<>();
         modules.add("Maths");
         modules.add("PoP");
         modules.add("Sys Arch");
-        createInputWindow(1, modules);
-    }
 
-    //0 would be to create a new module and 1 would be to add to an existing module
-    public static void createInputWindow(int indicator, ArrayList<String> modules){
-        JFrame window = new JFrame();
+        moduleDropDown = new JComboBox<>(modules.toArray());
+        addSession.add(moduleDropDown);
 
-        if(indicator == 0){
-            JPanel createModulePanel = new JPanel();
+        hoursInput = new JTextField(2);
+        JLabel hoursInputLabel = new JLabel("Hours");
 
-            JLabel createModuleLabel = new JLabel("Enter module name:");
-            JTextField createModuleInput = new JTextField(20);
+        minutesInput = new JTextField(2);
+        JLabel minutesInputLabel = new JLabel("Minutes");
 
-            createModulePanel.add(createModuleLabel);
-            createModulePanel.add(createModuleInput);
-            createModulePanel.setLayout(new FlowLayout());
+        JPanel hoursPanel = new JPanel();
+        hoursPanel.add(hoursInputLabel);
+        hoursPanel.add(hoursInput);
 
-            window.add(createModulePanel);
-        }
+        JPanel minutesPanel = new JPanel();
+        minutesPanel.add(minutesInputLabel);
+        minutesPanel.add(minutesInput);
 
-        if(indicator == 1){
-            JComboBox<Object> moduleDropDown = new JComboBox<>(modules.toArray());
-            window.add(moduleDropDown);
+        JPanel timeInputPanel = new JPanel();
+        timeInputPanel.add(hoursPanel);
+        timeInputPanel.add(minutesPanel);
+        timeInputPanel.setLayout(new FlowLayout());
 
-            JTextField hoursInput = new JTextField(2);
-            JLabel hoursInputLabel = new JLabel("Hours");
+        addSession.add(timeInputPanel);
 
-            JTextField minutesInput = new JTextField(2);
-            JLabel minutesInputLabel = new JLabel("Minutes");
 
-            JPanel hoursPanel = new JPanel();
-            hoursPanel.add(hoursInputLabel);
-            hoursPanel.add(hoursInput);
+        // Adding the cardPanel on "home"
+        cardPanel.add(home, "1");
 
-            JPanel minutesPanel = new JPanel();
-            minutesPanel.add(minutesInputLabel);
-            minutesPanel.add(minutesInput);
+        // Adding the cardPanel on "addModule"
+        cardPanel.add(addModule, "2");
 
-            JPanel timeInputPanel = new JPanel();
-            timeInputPanel.add(hoursPanel);
-            timeInputPanel.add(minutesPanel);
-            timeInputPanel.setLayout(new FlowLayout());
+        // Adding the cardPanel on "cardPanel"
+        cardPanel.add(addSession, "3");
 
-            window.add(timeInputPanel);
-        }
 
-        JPanel buttonPanel = new JPanel();
 
-        JButton enterButton = new JButton("Enter");
-        JButton cancelButton = new JButton("Cancel");
-        buttonPanel.add(enterButton);
+        buttonPanel.setVisible(currentCard != 1);
+
+        cancelButton = new JButton("Cancel");
+        enterButton = new JButton("Enter");
+
         buttonPanel.add(cancelButton);
-        buttonPanel.setLayout(new FlowLayout());
-        window.add(buttonPanel);
+        buttonPanel.add(enterButton);
 
-        window.setLayout(new BoxLayout(window.getContentPane(), BoxLayout.Y_AXIS));
-        window.pack();
-        window.setVisible(true);
+        cancelButton.addActionListener(this);
+        enterButton.addActionListener(this);
+
+
+        // used to get content pane
+        getContentPane().add(cardPanel, BorderLayout.NORTH);
+
+        // used to get content pane
+        getContentPane().add(buttonPanel, BorderLayout.SOUTH);
     }
+
+    private void cancelPanelVisible(int card) {
+        buttonPanel.setVisible(card != 1);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == cancelButton) {
+            cl.first(cardPanel); //Changes panel to home panel
+
+            currentCard = 1;
+
+            cancelPanelVisible(currentCard);
+        } else if (e.getSource() == enterButton) {
+
+            if (currentCard == 2) {
+                String moduleName = createModuleInput.getText();
+
+                //Call Module()
+            }
+            else if (currentCard == 3) {
+                Object module = moduleDropDown.getSelectedItem();
+
+                String hours = hoursInput.getText();
+                String minutes = minutesInput.getText();
+
+                //Call session???
+            }
+
+            cl.first(cardPanel); //Changes panel to home panel
+
+            currentCard = 1;
+
+            cancelPanelVisible(currentCard);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        ModuleView demo = new ModuleView();
+
+        demo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        demo.setVisible(true);
+    }
+
+    //TODO:
+    // - Clear JTextFields when their panels are shown
+    // - Call module and session
+    // - Get list of modules and populate the drop down with these -> from state class
+    // - The drop down should be of type module
+    // - Should be Module module = moduleDropDown.getSelectedItem();
 }
