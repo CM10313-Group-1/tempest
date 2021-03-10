1f15ac950c3cf34294924e5a0dd9c4819bf23fec
Max Wood <45072525+bridges-wood@users.noreply.github.com>
2021-03-10 20:06:24 +0000

    Module refactor.

--- src/main/java/tempest/Module.java
+++ src/main/java/tempest/Module.java
@@ -1,8 +1,12 @@
 package tempest;
 
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.LinkedList;
+import java.util.UUID;
+
+import tempest.interfaces.CSVInterface;
 
 /**
  * Module class represents a module and stores its moduleid, name and a list of
@@ -10,20 +14,22 @@ import java.util.LinkedList;
  *
  */
 public class Module {
-    public String id;
-    public String name;
-    public LinkedList<StudySession> studySessions = new LinkedList<StudySession>();
+    private UUID id;
+    private String name;
+    private LinkedList<StudySession> studySessions = new LinkedList<StudySession>();
 
-    public Module() {
+    public Module(String name) {
+        this.id = UUID.randomUUID();
+        this.name = name;
     }
 
     public Module(String id, String name) {
-        this.id = id;
+        this.id = UUID.fromString(id);
         this.name = name;
     }
 
     public Module(String id, String name, LinkedList<StudySession> studySessions) {
-        this.id = id;
+        this.id = UUID.fromString(id);
         this.name = name;
         this.studySessions = studySessions;
     }
@@ -55,10 +61,48 @@ public class Module {
      * @return An array of rows.
      */
     public String[] toRows() {
-        String[] output = new String[studySessions.size()];
-        for (int i = 0; i < studySessions.size(); i++) {
-            output[i] = studySessions.get(i).toRow(this);
+        if (studySessions.size() > 0) {
+            String[] output = new String[studySessions.size()];
+            for (int i = 0; i < studySessions.size(); i++) {
+                output[i] = studySessions.get(i).toRow(this);
+            }
+            return output;
+        } else {
+            return new String[] { this.toBlankRow() };
         }
-        return output;
+    }
+
+    public String toBlankRow() {
+        return getID() + CSVInterface.DELIMITER + getName() + CSVInterface.DELIMITER;
+    }
+
+    public StudySession[] getStudySessions() {
+        return studySessions.toArray(new StudySession[studySessions.size()]);
+    }
+
+    public String getID() {
+        return id.toString();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (obj == null)
+            return false;
+
+        if (obj instanceof Module) {
+            Module other = (Module) obj;
+            boolean matchingIDs = this.id.equals(other.id);
+            boolean matchingNames = this.name.equals(other.name);
+            boolean matchingSessions = Arrays.deepEquals(this.getStudySessions(), other.getStudySessions());
+            return matchingIDs && matchingNames && matchingSessions;
+        } else {
+            return false;
+        }
     }
 }
