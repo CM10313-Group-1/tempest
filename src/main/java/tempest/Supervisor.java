2fd54f5d1fff0d33ab3ac3173614a380fe605c66
Drayz64 <dwtr20@bath.ac.uk>
2021-03-15 00:30:25 +0000

    Added some comments to supervisor

--- src/main/java/tempest/Supervisor.java
+++ src/main/java/tempest/Supervisor.java
@@ -8,11 +8,15 @@ public class Supervisor {
     // TODO UserInterface
 
     private void onStart() {
-
+        // Run CSV code first
+        // - Check if CSV empty/nothing to load
+        // - Create modules using state
+        // - Load all the study session for these modules
+        // Start GUI last - by calling new ModuleView()
     }
 
     private void onClose() {
-
+        // Detect the GUI closing?
     }
 
     public static void main(String[] args) {
