dcfd7a2052bffff539035d617845e5405be3c6ee
Drayz64 <dwtr20@bath.ac.uk>
2021-03-18 00:08:11 +0000

    Build didn't start so trying if a commit will fix it

--- src/main/java/tempest/ModuleView.java
+++ src/main/java/tempest/ModuleView.java
@@ -25,6 +25,7 @@ import javax.swing.JTextField;
 // - Change the code to be dynamic
 // - Add a check so the user can't enter a session > 24hrs
 // - Add a check so the user can't enter study sessions in a day which add up to be > 24hrs
+// -
 
 /**
  * ModuleView is responsible for creating the GUIs for the home page, adding a
