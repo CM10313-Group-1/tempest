847609118ac0157e2084d76ffcbb0d1de17082a4
Drayz64 <dwtr20@bath.ac.uk>
2021-03-26 22:53:56 +0000

    Added page code and tidied up some code in ViewManager w/ help from IntelliJ

--- src/main/java/tempest/ui/pages/Page.java
+++ src/main/java/tempest/ui/pages/Page.java
@@ -1,34 +1,15 @@
 package tempest.ui.pages;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+        import java.awt.event.ActionEvent;
+        import java.awt.event.ActionListener;
 
-import javax.swing.JPanel;
+        import tempest.ui.View;
 
 /**
  * All page classes should extend this class
  */
-public abstract class Page implements ActionListener {
-
-    /**
-     * Ensures all pages have a getName() method
-     *
-     * Used when adding all pages to the cardLayout in the manager
-     *
-     * The name of the page is used to identify the page in the cardLayout
-     *
-     * @return String - Name of the page
-     */
-    public abstract String getName();
-
-    /**
-     * Ensures all pages have a getPanel() method
-     *
-     * May also be useful if need to call page.getPanel()
-     *
-     * @return A JPanel holding all the necessary components for that specific page
-     */
-    public abstract JPanel getPanel();
+public abstract class Page extends View implements ActionListener {
+    private static final long serialVersionUID = -7384127634444815527L;
 
     // Can't be abstract because there might be a page class that doesn't implement
     // ActionListener
@@ -42,4 +23,6 @@ public abstract class Page implements ActionListener {
     public void clearInput() {
         System.err.println("One of the page classes your using needs an clearInput() method");
     }
+
 }
+
