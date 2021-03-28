307edff787862eed83e41537c7343c8f0b83ff00
Max Wood <bridges.wood@gmail.com>
2021-03-28 15:13:26 +0100

    Add charts to chart view.

--- src/main/java/tempest/ui/components/charts/Chart.java
+++ src/main/java/tempest/ui/components/charts/Chart.java
@@ -2,13 +2,22 @@ package tempest.ui.components.charts;
 
 import org.jfree.chart.JFreeChart;
 
+import tempest.State;
 import tempest.ui.View;
+import tempest.ui.ViewManager;
 
 /**
  * Generic chart class.
  */
 public abstract class Chart extends View {
   private static final long serialVersionUID = -2648755806373669402L;
+  public State state;
+  public ViewManager<Chart> vm;
+
+  public Chart(State state, ViewManager<Chart> manager) {
+    this.state = state;
+    this.vm = manager;
+  }
 
   public abstract JFreeChart getChart();
 }
--- src/main/java/tempest/ui/pages/ChartViewPage.java
+++ src/main/java/tempest/ui/pages/ChartViewPage.java
@@ -7,25 +7,30 @@ import javax.swing.JPanel;
 import tempest.State;
 import tempest.ui.GUIManager;
 import tempest.ui.ViewManager;
+import tempest.ui.components.BackButton;
 import tempest.ui.components.LinkButton;
 import tempest.ui.components.charts.BarChart;
 import tempest.ui.components.charts.Chart;
+import tempest.ui.components.charts.ChartTypes;
 import tempest.ui.components.charts.LineChart;
 import tempest.ui.components.charts.PieChart;
 
 public class ChartViewPage extends Page {
   private static final long serialVersionUID = -7397536728116537358L;
   private final GUIManager manager;
-  private final State state;
   private Chart[] charts;
   private ViewManager<Chart> vm;
 
+  private LinkButton barChartLink = new LinkButton("Bar Chart", ChartTypes.BAR, this);
+  private LinkButton lineChartLink = new LinkButton("Line Chart", ChartTypes.LINE, this);
+  private LinkButton pieChartLink = new LinkButton("Pie Chart", ChartTypes.PIE, this);
+  private BackButton backButton;
+
   public ChartViewPage(State state, GUIManager guiManager) {
     super();
-    this.state = state;
     this.manager = guiManager;
 
-    this.charts = new Chart[] { new BarChart(), new LineChart(), new PieChart() };
+    this.charts = new Chart[] { new BarChart(state, vm), new LineChart(state, vm), new PieChart(state, vm) };
     vm = new ViewManager<Chart>(charts, charts[0]);
     this.add(vm);
     addNavButtons();
@@ -38,11 +43,19 @@ public class ChartViewPage extends Page {
 
   private void addNavButtons() {
     JPanel buttonPanel = new JPanel();
+    backButton = new BackButton(manager);
+
+    buttonPanel.add(barChartLink);
+    buttonPanel.add(lineChartLink);
+    buttonPanel.add(pieChartLink);
+    buttonPanel.add(backButton);
+    this.add(buttonPanel);
   }
 
   @Override
   public void actionPerformed(ActionEvent e) {
     LinkButton source = (LinkButton) e.getSource();
+    System.out.println(source.getDestination());
     vm.changeView(source.getDestination());
   }
 
