13497f31cd61c87d0bb7896799d130e306e28446
JoshRenton <78546327+JoshRenton@users.noreply.github.com>
2021-03-31 17:28:43 +0100

    Filled out pie chart class.

--- src/main/java/tempest/ui/components/charts/PieChart.java
+++ src/main/java/tempest/ui/components/charts/PieChart.java
@@ -2,10 +2,15 @@ package tempest.ui.components.charts;
 
 import javax.swing.JLabel;
 
-import org.jfree.data.general.Dataset;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
 
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.PieDataset;
 import tempest.State;
+import tempest.StudySession;
 import tempest.ui.ViewManager;
+import tempest.Module;
 
 public class PieChart extends Chart {
   private static final long serialVersionUID = 7074811797165362922L;
@@ -13,6 +18,12 @@ public class PieChart extends Chart {
   public PieChart(State state, ViewManager<Chart> manager) {
     super(state, manager);
     this.add(new JLabel(getName()));
+    this.add(createChart());
+  }
+
+  private ChartPanel createChart(){
+    PieDataset<String> dataset = generateDataset(state);
+    return new ChartPanel(ChartFactory.createPieChart("Pie Chart", dataset, true, true, false));
   }
 
   @Override
@@ -21,9 +32,16 @@ public class PieChart extends Chart {
   }
 
   @Override
-  public Dataset generateDataset(State state) {
-    // TODO Auto-generated method stub
-    return null;
+  public PieDataset<String> generateDataset(State state) {
+    DefaultPieDataset<String> dataset = new DefaultPieDataset<>();
+    for(Module module: state.getModules()){
+      int totalStudyTime = 0;
+      for(StudySession studySession: module.getStudySessions()){
+        totalStudyTime += studySession.duration.toMinutes();
+      }
+      dataset.setValue(module.getName(), totalStudyTime);
+    }
+    return dataset;
   }
 
 }
