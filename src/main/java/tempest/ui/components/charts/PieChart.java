e02bfb52db1dfc0d474d66521b4acf5e647a0d74
Drayz64 <dwtr20@bath.ac.uk>
2021-04-02 18:02:33 +0100

    Formatting

--- src/main/java/tempest/ui/components/charts/PieChart.java
+++ src/main/java/tempest/ui/components/charts/PieChart.java
@@ -16,45 +16,48 @@ import tempest.ui.ViewManager;
 import tempest.Module;
 
 public class PieChart extends Chart {
-  private static final long serialVersionUID = 7074811797165362922L;
+    private static final long serialVersionUID = 7074811797165362922L;
 
-  public PieChart(State state, ViewManager<Chart> manager) {
-    super(state, manager);
-    this.add(new JLabel(getName()));
-    this.add(createChart());
-  }
-
-  private ChartPanel createChart(){
-    // TODO: Could get the label to show hours and minutes.
-    PieDataset<String> dataset = generateDataset(state);
-    ChartPanel pieChart = new ChartPanel(ChartFactory.createPieChart("Pie Chart", dataset, true, true, false));
-    // Changes the label formatting to allow minutes to be shown.
-    PieSectionLabelGenerator labelGenerator = new StandardPieSectionLabelGenerator("{0} = {1} mins");
-    PiePlot plot = (PiePlot) pieChart.getChart().getPlot();
-    plot.setLabelGenerator(labelGenerator);
-    return pieChart;
-  }
-
-  @Override
-  public String getName() {
-    return ChartTypes.PIE;
-  }
-
-  /**
-   * Creates the dataset used by the pie chart.
-   * @param state
-   * @return The dataset consisting of module names and total study time in minutes
-   */
-  public PieDataset<String> generateDataset(State state) {
-    DefaultPieDataset<String> dataset = new DefaultPieDataset<>();
-    for(Module module: state.getModules()){
-      int totalStudyTime = 0;
-      for(StudySession studySession: module.getStudySessions()){
-        totalStudyTime += studySession.duration.toMinutes();
-      }
-      dataset.setValue(module.getName(), totalStudyTime);
+    public PieChart(State state, ViewManager<Chart> manager) {
+        super(state, manager);
+        this.add(new JLabel(getName()));
+        this.add(createChart());
     }
-    return dataset;
-  }
+
+    private ChartPanel createChart() {
+        // TODO: Could get the label to show hours and minutes.
+
+        PieDataset<String> dataset = generateDataset(state);
+        ChartPanel pieChart = new ChartPanel(ChartFactory.createPieChart("Pie Chart", dataset, true, true, false));
+
+        // Changes the label formatting to allow minutes to be shown.
+        PieSectionLabelGenerator labelGenerator = new StandardPieSectionLabelGenerator("{0} = {1} mins");
+        PiePlot plot = (PiePlot) pieChart.getChart().getPlot();
+        plot.setLabelGenerator(labelGenerator);
+        return pieChart;
+    }
+
+    @Override
+    public String getName() {
+        return ChartTypes.PIE;
+    }
+
+    /**
+     * Creates the dataset used by the pie chart.
+     *
+     * @param state Instance of state
+     * @return The dataset consisting of module names and total study time in minutes
+     */
+    public PieDataset<String> generateDataset(State state) {
+        DefaultPieDataset<String> dataset = new DefaultPieDataset<>();
+        for (Module module : state.getModules()) {
+            int totalStudyTime = 0;
+            for (StudySession studySession : module.getStudySessions()) {
+                totalStudyTime += studySession.duration.toMinutes();
+            }
+            dataset.setValue(module.getName(), totalStudyTime);
+        }
+        return dataset;
+    }
 
 }
