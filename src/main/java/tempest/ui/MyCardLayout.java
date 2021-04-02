d30385fb29d92f63e473400daa73c2c72870cbe6
Drayz64 <dwtr20@bath.ac.uk>
2021-04-02 15:02:24 +0100

    Small changes

--- src/main/java/tempest/ui/MyCardLayout.java
+++ src/main/java/tempest/ui/MyCardLayout.java
@@ -15,19 +15,9 @@ public class MyCardLayout extends CardLayout {
         if (current.getName() != null) {
             String name = current.getName();
 
-            System.out.println("Name : " + name);
+            //System.out.println("Name : " + name);
 
-            // Doesn't resize when line chart pressed
-
-//            if (name.equals(PageNames.CHART_VIEW) || name.equals(PageNames.DELETE_SESSION)) {
-//                Insets insets = parent.getInsets();
-//                Dimension pref = current.getPreferredSize();
-//                pref.width += insets.left + insets.right;
-//                pref.height += insets.top + insets.bottom;
-//                return pref;
-//            }
-
-            // Resizes correctly with graphs - but it means that when in Chart_View the box is very packed & tiny
+            // Resizes correctly with graphs - but it means that when in Chart_View the box is very small
 
             if (name.equals(PageNames.CHART_VIEW) || name.equals(PageNames.DELETE_SESSION) || name.equals("PieChart") || name.equals("LineChart") || name.equals("BarChart")) {
                 Insets insets = parent.getInsets();
@@ -38,8 +28,9 @@ public class MyCardLayout extends CardLayout {
             }
         }
 
-        System.out.println(":(");
+        //System.out.println("Default");
 
+        // The default frame size for panels
         return new Dimension(500, 150);
     }
 
