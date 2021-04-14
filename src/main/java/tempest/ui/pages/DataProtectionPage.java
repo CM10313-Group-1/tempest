c70c0ff0d3acef9cde7a72bb5147b9e17625d94e
Drayz64 <dwtr20@bath.ac.uk>
2021-04-14 18:50:22 +0100

    Some more spaces

--- src/main/java/tempest/ui/pages/DataProtectionPage.java
+++ src/main/java/tempest/ui/pages/DataProtectionPage.java
@@ -23,16 +23,20 @@ public class DataProtectionPage extends Page {
     private void setupUI(){
         JPanel backPanel = new JPanel();
         JPanel textPanel = new JPanel();
+
         text = "<ul><li>All data collected is stored in a CSV file called store.csv\n" +
                 "<li> Any data inputted by the user from within the program will be stored within this file\n" +
                 "<li>The data is local to each user therefore can only be visible/accessed by the user\n" +
                 "<li>Altering the contents of this file may affect the program and cause errors <ul>";
+
         text = "<html><font size='2'>" + text + "</font></html>";
+
         JLabel textLabel = new JLabel();
         textLabel.setText(text);
+        textPanel.add(textLabel);
+
         backButton = new BackButton(manager);
         backPanel.add(backButton);
-        textPanel.add(textLabel);
 
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         this.add(textPanel);
