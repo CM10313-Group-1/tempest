4f5ed0ca16d40ffcf06933dd2dc8168b010fc7d6
ihussain0910 <78728394+ihussain0910@users.noreply.github.com>
2021-04-16 17:32:36 +0100

    Changes

--- src/main/java/tempest/ui/pages/DataProtectionPage.java
+++ src/main/java/tempest/ui/pages/DataProtectionPage.java
@@ -24,14 +24,14 @@ public class DataProtectionPage extends Page {
         JPanel backPanel = new JPanel();
         JPanel textPanel = new JPanel();
 
-        text = "<ul><li>All data collected (not including goals) is stored in a CSV file called store.csv\n" +
+        text = "<ul><li>All data collected (not including goals) is stored in a CSV file called store.csv</li>"+
 
-                "<li> Any data inputted by the user from within the program will be stored within this file\n" +
-                "<li>The data is local to each user therefore can only be visible/accessed by the user\n" +
-                "<li>Altering the contents of the csv file may affect the program and cause errors" +
-                "<li>Deleting the csv file or changing the location of the csv file may also affect the program" +
-                "<li> Goals will be stored using serialisation, meaning they will be stored within the program itself" +
-                "<li> This means that goals can not be accessed or be visible outside of the program<ul>" ;
+                "<li> Any data inputted by the user from within the program will be stored within this file</li>" +
+                "<li>The data is local to each user therefore can only be visible/accessed by the user</li>" +
+                "<li>Altering the contents of the csv file may affect the program and cause errors</li>" +
+                "<li>Deleting the csv file or changing the location of the csv file may also affect the program</li>" +
+                "<li> Goals will be stored using serialisation, meaning they will be stored within the program itself</li>" +
+                "<li> This means that goals can not be accessed or be visible outside of the program</li></ul>" ;
 
         text = "<html><font size='2'>" + text + "</font></html>";
 
