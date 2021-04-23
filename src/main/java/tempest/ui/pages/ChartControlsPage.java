533ec1d36ccec1d52982eb3f5b48fa065b21ef69
Drayz64 <dwtr20@bath.ac.uk>
2021-04-23 20:39:12 +0100

    Added back button changes to ChartControlsPage

--- src/main/java/tempest/ui/pages/ChartControlsPage.java
+++ src/main/java/tempest/ui/pages/ChartControlsPage.java
@@ -5,11 +5,9 @@ import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JPanel;
 
 import tempest.State;
 import tempest.ui.GUIManager;
-import tempest.ui.components.BackButton;
 import tempest.ui.components.ChartControls;
 import tempest.ui.components.ModuleControl;
 
@@ -29,12 +27,10 @@ public class ChartControlsPage extends Page implements ActionListener {
     this.removeAll();
     this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
     this.add(controls = new ChartControls(state));
-    JPanel buttonPanel = new JPanel();
-    buttonPanel.add(resetButton);
-    buttonPanel.add(new BackButton(manager));
+    backPanel.add(resetButton);
     resetButton.addActionListener(this);
 
-    this.add(buttonPanel);
+    this.add(backPanel);
   }
 
   public void updateState(State state) {
